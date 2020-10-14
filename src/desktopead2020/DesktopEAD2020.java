25f9c58872391a7c80edd13bd02278da384de563
alexandresturmerwolf <senhagit>
2020-10-14 20:56:35 -0300

    Adicionei conexão com o banco de dados

--- src/desktopead2020/DesktopEAD2020.java
+++ src/desktopead2020/DesktopEAD2020.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package desktopead2020;
 
-/**
- *
- * @author wolfi
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DesktopEAD2020 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("A vaca morreu");
+        // ============== Exemplo com postgres ==========================
+        //String nomeDriver = "org.postgresql.Driver";
+        //String localBancoDados = "jdbc:postgresql://localhost:5432/postgres";
+        //String usuario = "postgres";
+        //String senha = "admin";
+        
+        // ============== Exemplo com mysql/mariadb =======================
+        String nomeDriver = "org.mariadb.jdbc.Driver";
+        String localBancoDados = "jdbc:mariadb://localhost:3306/bancodados";
+        String usuario = "root";
+        String senha = "admin";
+
+        Connection conexao = null;
+
+        try {
+
+            Class.forName(nomeDriver).newInstance();
+            conexao = DriverManager.getConnection(localBancoDados, usuario, senha);
+
+            if (conexao != null) {
+                Statement st = conexao.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM alunos");
+
+                while (rs.next()) {
+                    System.out.println(rs.getString("codigo") + "   " + rs.getString("nome")+ "   " + rs.getString("endereco"));
+                }
+
+            } else {
+                System.out.println("Problemas na conexão com o banco de dados!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
     }
     
 }
